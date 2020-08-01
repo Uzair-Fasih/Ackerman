0885a00ba71e4fdb8a3d63edd582ff188a8905ca
Uzair Fasih <uzair.fassi@gmail.com>
2020-08-01 12:36:14 +0530

    Added a new home screen

---
+++ src/screens/nHome/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
